c81856955fd2ef89450d9c241fce94a6ac1d82f4
Seme87 <massimomonteleone@hotmail.com>
2022-10-21 16:39:15 +0200

    fix function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // NOME E COGNOME UTENTE
 const name = document.getElementById("name").value = "Massimo Monteleone";
 // KM PERCORSI
-document.getElementById("km").value = "100";
+// document.getElementById("km").value = "100";
 
 
 const btnGenerator= document.getElementById("btn-geneator");
-btnGenerator.addEventListener('click, ' function{
-    document.getElementById("name-surname").innerHTML= "name";
+btnGenerator.addEventListener("click ," function() {
+    document.getElementById("name-surname").innerHTML= name;
 })
