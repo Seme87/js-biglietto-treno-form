d4f21353404615ee510ea8f9239719b7bcd801b6
Seme87 <massimomonteleone@hotmail.com>
2022-10-21 17:11:39 +0200

    aggiunge alla funzione click i km percorsi

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-// NOME E COGNOME UTENTE
-const name = document.getElementById("name").value = "Massimo Monteleone";
-// KM PERCORSI
-// document.getElementById("km").value = "100";
+
 
 
 const btnGenerator= document.getElementById("btn-geneator");
-btnGenerator.addEventListener("click ," function() {
+
+    btnGenerator.addEventListener("click", function() {
+    let name = document.getElementById("name").value
     document.getElementById("name-surname").innerHTML= name;
+    let km = document.getElementById('kilometers').value
+    document.getElementById('km').innerHTML=km;
+
 })
